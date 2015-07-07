1779f3592c6253acfdd27257b86d5f8cb1f62968
Rynn Wang <rynn.wang@outlook.com>
2015-07-07 18:09:47 +0800

    no message

--- Beyova.SendCloud.SDK/SendCloudClient/Email.cs
+++ Beyova.SendCloud.SDK/SendCloudClient/Email.cs
@@ -16,90 +16,6 @@ namespace Beyova.SendCloud.SDK
         /// Sends the mail.
         /// </summary>
         /// <param name="from">From.</param>
-        /// <param name="toList">To list.</param>
-        /// <param name="ccList">The cc list.</param>
-        /// <param name="bccList">The BCC list.</param>
-        /// <param name="subject">The subject.</param>
-        /// <param name="html">The HTML.</param>
-        /// <param name="replyTo">The reply to.</param>
-        /// <param name="headers">The headers.</param>
-        /// <param name="files">The files.</param>
-        /// <param name="smtpApi">The SMTP API.</param>
-        /// <param name="useMailList">if set to <c>true</c> [use mail list].</param>
-        /// <param name="label">The label.</param>
-        /// <param name="gzip">if set to <c>true</c> [gzip].</param>
-        /// <returns>List&lt;System.String&gt;.</returns>
-        public List<string> SendMail(MailAddress from, MailAddress[] toList, MailAddress[] ccList, MailAddress[] bccList, string subject, string html, string replyTo, Dictionary<string, string> headers, string files, string smtpApi, bool useMailList = false, int? label = null, bool gzip = false)
-        {
-            const string module = "mail";
-            const string action = "send";
-            const string responsePropertyName = "email_id_list";
-
-            try
-            {
-                var parameters = new Dictionary<string, string>();
-                #region Parameter prepare
-
-                parameters.Add("from", from.Address);
-                parameters.Add("to", ToMailAddress(toList));
-                parameters.Add("subject", subject);
-                parameters.Add("html", html);
-                parameters.Add("bcc", ToMailAddress(bccList));
-                parameters.Add("cc", ToMailAddress(ccList));
-
-                if (!string.IsNullOrWhiteSpace(from.DisplayName))
-                {
-                    parameters.Add("fromname", from.DisplayName);
-                }
-
-                if (!string.IsNullOrWhiteSpace(replyTo))
-                {
-                    parameters.Add("replyto", replyTo);
-                }
-
-                if (label != null)
-                {
-                    parameters.Add("label", label.ToString());
-                }
-
-                if (headers != null)
-                {
-                    parameters.Add("headers", headers.ToJson(false));
-                }
-
-                if (!string.IsNullOrWhiteSpace(files))
-                {
-                    parameters.Add("files", files);
-                }
-
-                if (!string.IsNullOrWhiteSpace(smtpApi))
-                {
-                    parameters.Add("x_smtpapi", smtpApi);
-                }
-
-                parameters.Add("resp_email_id", "true");
-                parameters.Add("use_maillist", useMailList.ToString().ToLowerInvariant());
-                parameters.Add("gzip_compress", gzip.ToString().ToLowerInvariant());
-
-                #endregion
-
-                var response = Invoke(module, action, HttpConstants.HttpMethod.Post, parameters, responsePropertyName);
-                return response.Value<List<string>>();
-            }
-            catch (Exception ex)
-            {
-                throw ex.Handle("SendMail", new
-                {
-                    from = from.Address,
-                    to = ToMailAddress(toList)
-                });
-            }
-        }
-
-        /// <summary>
-        /// Sends the mail.
-        /// </summary>
-        /// <param name="from">From.</param>
         /// <param name="to">To.</param>
         /// <param name="templateName">Name of the template.</param>
         /// <param name="replacements">The replacements.</param>
@@ -108,9 +24,9 @@ namespace Beyova.SendCloud.SDK
         /// <param name="files">The files.</param>
         /// <param name="smtpApi">The SMTP API.</param>
         /// <param name="label">The label.</param>
-        /// <param name="gzip">if set to <c>true</c> [gzip].</param>
+        /// <param name="usingGZip">if set to <c>true</c> [using g zip].</param>
         /// <returns>List&lt;System.String&gt;.</returns>
-        public List<string> SendMail(MailAddress from, MailAddress to, string templateName, Dictionary<string, string> replacements, string replyTo, Dictionary<string, string> headers, string files, string smtpApi, int? label = null, bool gzip = false)
+        public List<string> SendMail(MailAddress from, MailAddress to, string templateName, Dictionary<string, string> replacements, string replyTo, Dictionary<string, string> headers, string files, string smtpApi, int? label = null, bool usingGZip = false)
         {
             try
             {
@@ -118,7 +34,7 @@ namespace Beyova.SendCloud.SDK
                 substitution.To.Add(to.Address);
                 substitution.Add(to.Address, replacements);
 
-                return SendMail(from, templateName, substitution, replyTo, headers, files, smtpApi, label, gzip);
+                return SendMail(from, templateName, substitution, replyTo, headers, files, smtpApi, label, usingGZip);
             }
             catch (Exception ex)
             {
@@ -130,8 +46,21 @@ namespace Beyova.SendCloud.SDK
             }
         }
 
+        /// <summary>
+        /// Sends the mail.
+        /// </summary>
+        /// <param name="from">From.</param>
+        /// <param name="templateName">Name of the template.</param>
+        /// <param name="substitution">The substitution.</param>
+        /// <param name="replyTo">The reply to.</param>
+        /// <param name="headers">The headers.</param>
+        /// <param name="files">The files.</param>
+        /// <param name="smtpApi">The SMTP API.</param>
+        /// <param name="label">The label.</param>
+        /// <param name="usingGZip">if set to <c>true</c> [using g zip].</param>
+        /// <returns>List&lt;System.String&gt;.</returns>
         public List<string> SendMail(MailAddress from, string templateName, TemplateEmailSubstitution substitution, string replyTo, Dictionary<string, string> headers, string files,
-            string smtpApi, int? label = null, bool gzip = false)
+            string smtpApi, int? label = null, bool usingGZip = false)
         {
             const string module = "mail";
             const string action = "send_template";
@@ -139,13 +68,15 @@ namespace Beyova.SendCloud.SDK
 
             try
             {
-                var parameters = new Dictionary<string, string>();
                 #region Parameter prepare
 
-                parameters.Add("from", from.Address);
-                parameters.Add("template_invoke_name", templateName);
-                parameters.Add("substitution_vars", substitution.ToJson());
-
+                var parameters = new Dictionary<string, string>
+                {
+                    {"from", @from.Address},
+                    {"template_invoke_name", templateName},
+                    {"substitution_vars", substitution.ToJson()}
+                };
+                
                 if (!string.IsNullOrWhiteSpace(from.DisplayName))
                 {
                     parameters.Add("fromname", from.DisplayName);
@@ -177,7 +108,7 @@ namespace Beyova.SendCloud.SDK
                 }
 
                 parameters.Add("resp_email_id", "true");
-                parameters.Add("gzip_compress", gzip.ToString().ToLowerInvariant());
+                parameters.Add("gzip_compress", usingGZip.ToString().ToLowerInvariant());
 
                 #endregion
 
