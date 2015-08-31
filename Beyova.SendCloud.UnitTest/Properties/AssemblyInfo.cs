dbf284716d12b1fd96417c1287a76777f8461e1d
Rynn Wang <rynn.wang@outlook.com>
2015-08-31 17:40:03 +0800

    no message

--- Beyova.SendCloud.UnitTest/Properties/AssemblyInfo.cs
+++ Beyova.SendCloud.UnitTest/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Beyova.SendCloud.SDK")]
-[assembly: AssemblyDescription("0.4.0")]
+[assembly: AssemblyTitle("Beyova.SendCloud.UnitTest")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Beyova Co.,Ltd.")]
-[assembly: AssemblyProduct("Beyova.SendCloud.SDK")]
-[assembly: AssemblyCopyright("Copyright © Beyova 2015")]
+[assembly: AssemblyCompany("EF")]
+[assembly: AssemblyProduct("Beyova.SendCloud.UnitTest")]
+[assembly: AssemblyCopyright("Copyright © EF 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("72e5315a-a955-4c4b-a991-7e33d400de69")]
+[assembly: Guid("dc7f30f1-d8be-4688-bd59-4179fb229078")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-//[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Beyova.SendCloud.UnitTest/UnitTest1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using Beyova.SendCloud.SDK;
+using Beyova.SendCloud.SDK.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Beyova.SendCloud.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private string apiTriggerUser = "appior_noreply";
+        private string apiBulkUser = "Appior_Bulk";
+        private string apiKey = "ugenXIZo7haFEsGd";
+
+        [TestMethod]
+        public void TestTriggerAccount()
+        {
+            SendCloudClient client = new SendCloudClient(apiTriggerUser, apiKey);
+            var result = client.SendMail(new MailAddress("noreply@appior.com", "Appior"), new MailAddress("rynn.wang@outlook.com"), "WelcomeToAppior", new Dictionary<string, string>
+            {
+                { "email", "Rynn Wang" },
+                {"resetUrl","url"}
+            });
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Any());
+        }
+
+        [TestMethod]
+        public void TestBulkAccount()
+        {
+            SendCloudClient client = new SendCloudClient(apiBulkUser, apiKey);
+            var result = client.SendMail(new MailAddress("noreply@appior.com", "Appior"), new MailAddress("rynn.wang@outlook.com"), "AppiorProjectMemberInvitation", new Dictionary<string, string>
+            {
+                { "email", "Rynn Wang" },
+                {"currentUser","ANYONE"},
+                {"invitationUrl","invitationUrlLLL"}
+            });
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Any());
+        }
+
+        [TestMethod]
+        public void TestGetTemplate()
+        {
+            SendCloudClient client = new SendCloudClient(apiTriggerUser, apiKey);
+            var result = client.QueryTemplates(new EmailTemplateCriteria()
+        {
+        });
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Any());
+        }
+
+        [TestMethod]
+        public void TestAddTemplate()
+        {
+            SendCloudClient client = new SendCloudClient(apiTriggerUser, apiKey);
+            var result = client.AddTemplate(new EmailTemplate
+            {
+
+            });
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result > 0);
+        }
+    }
+}
