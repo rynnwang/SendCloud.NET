3419727fb6466def634f40bd706c8cb560a9d196
Rynn Wang <rynn.wang@ef.com>
2016-05-11 17:58:16 +0800

    no message

--- Beyova.SendCloud.SDK/Properties/AssemblyInfo.cs
+++ Beyova.SendCloud.SDK/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using Beyova.ProgrammingIntelligence;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: BeyovaComponent("Beyova.SendCloud", "0.5.0")]
+[assembly: BeyovaComponent("Beyova.SendCloud", "0.5.2")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -34,5 +34,5 @@ using Beyova.ProgrammingIntelligence;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
